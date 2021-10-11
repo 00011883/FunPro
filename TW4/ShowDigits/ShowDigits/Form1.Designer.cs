20a399325a551cdf70a33c59987120618c27e86a
00011883 <wiut00011883@gmail.com>
2021-10-11 19:22:03 +0500

    feat: add fourth task

---
+++ TW4/ShowDigits/ShowDigits/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ShowDigits
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx_input = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbx_input
+            // 
+            this.tbx_input.Location = new System.Drawing.Point(350, 124);
+            this.tbx_input.Name = "tbx_input";
+            this.tbx_input.Size = new System.Drawing.Size(100, 20);
+            this.tbx_input.TabIndex = 0;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(363, 166);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 1;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.tbx_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx_input;
+        private System.Windows.Forms.Button btn_submit;
+    }
+}
+
---
+++ TW4/ShowDigits/ShowDigits/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShowDigits
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            string input = tbx_input.Text;
+            string result = "";
+            
+            char[] array = input.ToCharArray();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                result += array[i].ToString();
+            }
+
+            MessageBox.Show(result);
+        }
+    }
+}
