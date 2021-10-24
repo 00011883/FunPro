2e7693f3b38807e540fc991888f60fd293aae277
00011883 <00011883@gmail.com>
2021-10-25 01:05:19 +0500

    feat: 2st task

---
+++ TW5/MirroredArrays/MirroredArrays/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MirroredArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx_input = new System.Windows.Forms.TextBox();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbx_input
+            // 
+            this.tbx_input.Location = new System.Drawing.Point(134, 153);
+            this.tbx_input.Name = "tbx_input";
+            this.tbx_input.Size = new System.Drawing.Size(100, 20);
+            this.tbx_input.TabIndex = 0;
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(117, 128);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(134, 13);
+            this.lbl_name.TabIndex = 1;
+            this.lbl_name.Text = "See the reversed input text";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(146, 179);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 2;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 327);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.tbx_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx_input;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Button btn_submit;
+    }
+}
+
---
+++ TW5/MirroredArrays/MirroredArrays/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MirroredArrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            string input = tbx_input.Text;
+            string[] strArray = input.Split(',');
+            string[] strNewArray = new string[strArray.Length];
+
+            int j = 0;
+
+            for (int i = strArray.Length - 1; i >= 0; i--)
+            {
+                strNewArray[j] = strArray[i];
+                j++;
+                
+            }
+
+            MessageBox.Show(String.Join(",", strNewArray));
+
+
+        }
+    }
+}
