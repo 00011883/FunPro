b57f84bce6cab483f137555034c097c3b5a31bd8
00011883 <wiut00011883@gmail.com>
2021-11-22 01:35:08 +0500

    feat: SQL Database usage

---
+++ TW9/DbConnected/DbConnected/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace DbConnected
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.conn = new System.Data.OleDb.OleDbConnection();
+            this.btn_retrieve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // conn
+            // 
+            this.conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=E:\\Users\\00011883\\Desktop\\db.mdb";
+            // 
+            // btn_retrieve
+            // 
+            this.btn_retrieve.Location = new System.Drawing.Point(30, 21);
+            this.btn_retrieve.Name = "btn_retrieve";
+            this.btn_retrieve.Size = new System.Drawing.Size(331, 45);
+            this.btn_retrieve.TabIndex = 0;
+            this.btn_retrieve.Text = "Retrieve";
+            this.btn_retrieve.UseVisualStyleBackColor = true;
+            this.btn_retrieve.Click += new System.EventHandler(this.btn_retrieve_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 87);
+            this.Controls.Add(this.btn_retrieve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.OleDb.OleDbConnection conn;
+        private System.Windows.Forms.Button btn_retrieve;
+    }
+}
+
---
+++ TW9/DbConnected/DbConnected/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DbConnected
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_retrieve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sql = "select * from tb_teacher";
+                var cmd = new OleDbCommand(sql, conn);
+                conn.Open();
+                var rdr = cmd.ExecuteReader();
+                var result = "";
+                while (rdr.Read())
+                {
+                    result += $"Id: {rdr.GetInt32(0)}; First name: {rdr.GetString(1)}; Last name: {rdr.GetString(2)}; Address: {rdr.GetString(3)}; Phone: {rdr.GetString(4)}";
+                    result += "\n";
+                }
+
+                MessageBox.Show(result);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if ((conn.State != ConnectionState.Closed))
+                {
+                    conn.Close();
+                }
+            }
+
+        }
+    }
+}
